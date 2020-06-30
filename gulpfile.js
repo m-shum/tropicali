bc481e1050b6a11639f1995ecc3a1d202c6ac226
Michael Shumakov <hello@mshumakov.com>
2020-06-30 20:13:18 +0100

    added gulp tasks – images, fonts, html, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,51 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var cleanCss = require("gulp-clean-css")
+var sourcemaps = require("gulp-sourcemaps")
+const gulpSourcemaps = require('gulp-sourcemaps')
+var browserSync = require('browser-sync').create()
 
-gulp.task('default', async function() {
-  // place code for your default task here
-});
+sass.compiler = require('node-sass')
+
+gulp.task("sass", function(){
+    return gulp.src("SRC/css/app.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(
+            cleanCss({ 
+                compatibility: 'ie8' 
+            })
+        )
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream())
+})
+
+gulp.task("html", function(){
+    return gulp.src("SRC/index.html")
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("fonts", function(){
+    return gulp.src("SRC/fonts/*")
+        .pipe(gulp.dest("dist/fonts"))
+})
+
+gulp.task("images", function() {
+    return gulp.src("SRC/img/*")
+        .pipe(gulp.dest("dist/img"))
+})
+
+gulp.task("watch", function() {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    })
+    gulp.watch("SRC/index.html", ["html"]).on("change", browserSync.reload)
+    gulp.watch("SRC/css/app.scss", ["sass"])
+    gulp.watch("SRC/fonts/*", ["fonts"])
+    gulp.watch("SRC/img/*", ["images"])
+})
+
+gulp.task('default', ["html", "sass", "fonts", "images", "watch"])
